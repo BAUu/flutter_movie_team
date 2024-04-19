2110da17d831d9f002674dad6300cbfbb91462a2
jiwon Choi <jiwon31273@naver.com>
2024-04-19 12:51:31 +0900

    feat: data layer data_source add

--- lib/data/data_source/movie_detail_data_source.dart
+++ lib/data/data_source/movie_detail_data_source.dart
@@ -1,15 +1,27 @@
 import 'dart:convert';
-
-
 import 'package:http/http.dart' as http;
 
-class WeatherDataSource {
-  final _baseUrl = 'https://api.themoviedb.org/3/movie/';
-    final _apiKey = 'a8e679e8b1b0eb19173daf3b989e2608';
+import '../dto/movie_detail_dto.dart';
+import '../dto/movie_playing_dto.dart';
+
+class MovieDataSource {
+  final _baseUrl = 'https://api.themoviedb.org/3';
+  final _apiKey = 'a8e679e8b1b0eb19173daf3b989e2608';
 
   Future<MovieDetailDto> getMovieDetailDto(num movieId) async {
-    final response = await http.get(Uri.parse('_baseUrl/$movieId?$_apiKey&language=ko-KR'));
+    final response = await http.get(Uri.parse('$_baseUrl/movie/$movieId?$_apiKey&language=ko-KR'));
 
     return MovieDetailDto.fromJson(jsonDecode(response.body));
+  }
+
+  Future<MoviePlayingDto> getPlayingMovie() async {
+    final response = await http.get(Uri.parse('$_baseUrl/movie/now_playing?language=en-US&page=1'));
+
+    return MoviePlayingDto.fromJson(jsonDecode(response.body));
+  }
+
+
+  Future<MovieSearchDto> getMovieSearch(String title) async {
+    final response = await http.get(Uri.parse('$_baseUrl/search/movie/now_playing?language=en-US&page=1'));
   }
 }
